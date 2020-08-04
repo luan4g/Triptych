bad5c98bb76ec3191caa1fad2100360c9f6bf653
Luan Ferreira <luan.ferreira.mads@gmail.com>
2020-08-03 22:50:05 -0300

    Add Page Settings

--- mobile/src/pages/Settings/index.tsx
+++ mobile/src/pages/Settings/index.tsx
@@ -1,19 +1,70 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 function Settings() {
   return(
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>
+          Settings
+        </Text>
+      </View>
+      <View style={styles.settings}>
+        <View style={styles.selection}>
+          <Text style={styles.selectionText}>Account</Text>
+        </View>
+        <View style={styles.selection}>
+          <Text style={styles.selectionText}>About Us</Text>
+        </View>
+        <View style={[styles.selection, styles.logOutCont]}>
+          <Text style={styles.logOut}>LogOut</Text>
+          <FontAwesome5 name="sign-out-alt" size={16} color="#F17A7A" />
+        </View>
+      </View>
     </View>
   )
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+  },
+  header: {
+    width: '100%',
     alignItems: 'center',
-    backgroundColor: '#fff'
+    flexDirection: 'row',
+    padding: 24,
+    backgroundColor: '#fff',
+    elevation: 5,
+  },
+  title: {
+    marginRight: 'auto',
+    fontSize: 16,
+    fontFamily: 'Roboto_Bold'
+  },
+  settings: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  selection: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderColor: '#eee'
+  },
+  selectionText: {
+    fontSize: 16,
+    fontFamily: 'Roboto_Bold',
+    color: '#000',
+  },
+  logOut: {
+    fontSize: 16,
+    fontFamily: 'Roboto_Bold',
+    color: '#F17A7A',
+    marginRight: 8,
+  },
+  logOutCont: {
+    alignItems: 'center',
+    flexDirection: 'row',
   }
 })
 
